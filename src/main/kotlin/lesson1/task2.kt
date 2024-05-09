6bee1844d1a8e06e4a2321b3b7aaec510dc938cd
Anastasia Tim <codex.brigadir@gmail.com>
2024-05-10 01:33:15 +0300

    KS-1-2

---
+++ src/main/kotlin/lesson1/task2.kt
@@ -0,0 +1,17 @@
+package lesson1
+
+fun main() {
+
+    var shoppingList: Int = 75
+    val summaryText: String = "Спасибо, что выбираете нас!"
+
+    println(shoppingList)
+    println(summaryText)
+
+    //var workers: Int = 2000
+    //println(workers)
+
+    var workers: Int = 1999
+    println(workers)
+
+}
