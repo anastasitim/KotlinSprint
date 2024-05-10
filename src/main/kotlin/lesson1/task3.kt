a3a4fa31f28276c79d491a5e401f520c53a8d604
Anastasia Tim <codex.brigadir@gmail.com>
2024-05-10 03:33:06 +0300

    KS-1-2

---
+++ src/main/kotlin/lesson1/task3.kt
@@ -0,0 +1,17 @@
+package lesson1
+
+fun main() {
+    val year: Int = 1961
+    var hourVzlet: Int = 9
+    var minuteVzlet: Int = 7
+
+    println(year)
+    println(hourVzlet)
+    println(minuteVzlet)
+
+    var hourPosadka: Int = 10
+    var minutePosadka: Int = 55
+
+    print("$hourPosadka:$minutePosadka")
+
+}
