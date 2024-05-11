e6dc80e5b464b60aeb4cbe524c1bbc6976e72606
Anastasia Tim <codex.brigadir@gmail.com>
2024-05-11 04:27:41 +0300

    KS-2-1

---
+++ src/main/kotlin/lesson2/task1.kt
@@ -0,0 +1,16 @@
+package lesson2
+
+fun main() {
+
+    val count = 4
+
+    val grade1 = 3.00
+    val grade2 = 4.00
+    val grade3 = 3.00
+    val grade4 = 5.00
+
+    val average = ((grade1+grade2+grade3+grade4)/count)
+
+    println(average)
+
+}
